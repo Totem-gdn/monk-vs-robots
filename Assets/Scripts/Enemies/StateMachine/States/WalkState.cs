a840d6376c3b7818c2759025702117283b85b600
ArthurITD <arthur.z@it-dimension.com>
2022-07-09 04:16:55 +0300

    Added air arena with new assets and robots checkpoints movement system

--- Assets/Scripts/Enemies/StateMachine/States/WalkState.cs
+++ Assets/Scripts/Enemies/StateMachine/States/WalkState.cs
@@ -7,17 +7,21 @@ using UnityEngine.AI;
 public class WalkState : BaseState
 {
     [SerializeField] private TargetType targetType;
+    [SerializeField] private bool isCheckpointsPath;
+    [Min(0)]
+    [SerializeField] private float checkpointTriggerDistance;
 
     private Collider targetCollider;
     private NavMeshAgent navMeshAgent;
     private bool isAgentExists = false;
+    private int localCheckpointIndex = -1;
 
     protected override void Awake()
     {
         base.Awake();
         targetCollider = DefineTargetCollider();
         isAgentExists = TryGetComponent<NavMeshAgent>(out navMeshAgent);
-        if(!isAgentExists)
+        if (!isAgentExists)
         {
             Debug.LogError($"NavMeshAgent not set to the WalkState of {name} enemy");
         }
@@ -27,13 +31,26 @@ public class WalkState : BaseState
     {
         if (isAgentExists && navMeshAgent.isOnNavMesh)
         {
-            SetNewDestination();
+            if (isCheckpointsPath && stateMachine.CurrentCheckpointIndex < stateMachine.CheckpointsPath.Count)
+            {
+                var distance = Vector3.Distance(transform.position, stateMachine.CurrentCheckpoint.position);
+
+                if (distance <= checkpointTriggerDistance)
+                {
+                    stateMachine.CheckpointReached();
+                    navMeshAgent.SetDestination(stateMachine.CurrentCheckpoint.position);
+                }
+            }
+            else
+            {
+                SetNewDestination();
+            }
         }
     }
 
     protected override void OnDisable()
     {
-        if(isAgentExists && navMeshAgent.isOnNavMesh)
+        if (isAgentExists && navMeshAgent.isOnNavMesh)
         {
             navMeshAgent.velocity = Vector3.zero;
             navMeshAgent.isStopped = true;
@@ -45,6 +62,10 @@ public class WalkState : BaseState
     {
         if (isAgentExists)
         {
+            if(isCheckpointsPath && localCheckpointIndex != stateMachine.CurrentCheckpointIndex)
+            {
+                navMeshAgent.SetDestination(stateMachine.CurrentCheckpoint.position);
+            }
             navMeshAgent.isStopped = false;
             animator.SetTrigger(Constants.WALK_ANIMATION_TRIGGER);
         }
